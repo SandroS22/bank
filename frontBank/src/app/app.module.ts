ac925237fe1ce396554050ae0329c078c61be8b6
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2023-12-31 13:04:48 -0300

    feat: some changes

--- frontBank/src/app/app.module.ts
+++ frontBank/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, LoginComponent],
+  declarations: [AppComponent, HomeComponent, LoginComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
