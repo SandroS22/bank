5daea1a64e40820058f7f15c4ab9d04457717a08
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2023-12-31 13:03:34 -0300

    feat: home page

--- frontBank/src/app/home/home.component.ts
+++ frontBank/src/app/home/home.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  saldo: number = 0.00;
 
+  deposit() {
+    this.saldo += 1.51;
+  }
 }
