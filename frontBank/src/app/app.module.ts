8ebeb935d71d548623d2720e1ab720d3b2996c7d
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-01-01 16:33:07 -0300

    refactor: remove navbar component from imports

--- frontBank/src/app/app.module.ts
+++ frontBank/src/app/app.module.ts
@@ -6,10 +6,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, LoginComponent, NavbarComponent],
+  declarations: [AppComponent, HomeComponent, LoginComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
