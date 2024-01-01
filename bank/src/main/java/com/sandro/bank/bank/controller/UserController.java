2b6f1664ecae62bb2b8e9c925f9beb22997b8625
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-01-01 18:20:38 -0300

    feat: create basic createUser method

--- bank/src/main/java/com/sandro/bank/bank/controller/UserController.java
+++ bank/src/main/java/com/sandro/bank/bank/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.sandro.bank.bank.controller;
 
+import java.util.Date;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sandro.bank.bank.model.User;
 import com.sandro.bank.bank.service.UserService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
@@ -22,6 +26,20 @@ public class UserController {
 	@GetMapping("/users")
 	public List<User> findAllUsers() {
 		return userService.findAll();
+	}
+
+	public HttpStatus createUser(@Valid Long cpf, String name, String email, Date birthDate) {
+		if (validateUser(cpf, name, email, birthDate)) {
+			return HttpStatus.CREATED;
+		} else {
+			return HttpStatus.FAILED_DEPENDENCY;
+		}
+	}
+
+	private Boolean validateUser(Long cpf, String name, String email, Date birthDate) {
+		// TODO Auto-generated method stub
+		return true;
+
 	}
 
 }
