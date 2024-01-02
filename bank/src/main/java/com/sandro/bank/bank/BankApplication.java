96fc620993ce74afe9a2e7c22dd4a09c66c39726
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-01-02 18:29:15 -0300

    chore: allow cross origins

--- bank/src/main/java/com/sandro/bank/bank/BankApplication.java
+++ bank/src/main/java/com/sandro/bank/bank/BankApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@CrossOrigin(origins = "https://localhost:4200", allowCredentials = "https://localhost:4200")
 public class BankApplication {
 
 	public static void main(String[] args) {
--- bank/src/main/java/com/sandro/bank/bank/controller/UserController.java
+++ bank/src/main/java/com/sandro/bank/bank/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.sandro.bank.bank.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
 	private UserService userService;
@@ -40,8 +43,10 @@ public class UserController {
 	}
 
 	@GetMapping("/register")
-	public String test() {
-		return "ok";
+	public List<String> test() {
+		List<String> lista = new ArrayList<>();
+		lista.add("Sandro");
+		return lista;
 	}
 
 }
