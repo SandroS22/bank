e02774094e8f34978fa67108bce63f9485c1cf0c
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-01-05 20:50:50 -0300

    feat: generate login service

--- frontBank/src/app/services/login.service.spec.ts
+++ frontBank/src/app/services/login.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { HttpServiceService } from './http-service.service';
+import { LoginService } from './login.service';
 
-describe('HttpServiceService', () => {
-  let service: HttpServiceService;
+describe('LoginService', () => {
+  let service: LoginService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(HttpServiceService);
+    service = TestBed.inject(LoginService);
   });
 
   it('should be created', () => {
