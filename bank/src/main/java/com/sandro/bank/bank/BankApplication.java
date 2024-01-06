f13b5fbdf34e2b051c9dd4dd065f0eebd78ffebf
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-01-05 21:45:23 -0300

    refactor: undo last commit

--- bank/src/main/java/com/sandro/bank/bank/BankApplication.java
+++ bank/src/main/java/com/sandro/bank/bank/BankApplication.java
@@ -2,10 +2,8 @@ package com.sandro.bank.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-@RequestMapping("/api")
 public class BankApplication {
 
 	public static void main(String[] args) {
--- bank/src/main/java/com/sandro/bank/bank/controller/UserController.java
+++ bank/src/main/java/com/sandro/bank/bank/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ import com.sandro.bank.bank.service.UserService;
 import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
