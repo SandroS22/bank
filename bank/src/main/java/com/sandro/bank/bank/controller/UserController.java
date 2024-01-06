beacaf30c91bfef25599336066eb59bb90843469
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-01-05 21:41:58 -0300

    refactor: move /api mapping to main

--- bank/src/main/java/com/sandro/bank/bank/controller/UserController.java
+++ bank/src/main/java/com/sandro/bank/bank/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +17,6 @@ import com.sandro.bank.bank.service.UserService;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
