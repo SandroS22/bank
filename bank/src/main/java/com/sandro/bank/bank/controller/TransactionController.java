41d558828bb17d51cb64c8efe5fceb3b904b3380
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-01-08 16:29:26 -0300

    feat: create implements find all get

--- bank/src/main/java/com/sandro/bank/bank/controller/TransactionController.java
+++ bank/src/main/java/com/sandro/bank/bank/controller/TransactionController.java
@@ -1,9 +1,13 @@
 package com.sandro.bank.bank.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sandro.bank.bank.entity.Transaction;
 import com.sandro.bank.bank.service.TransactionService;
 
 @RestController
@@ -12,4 +16,9 @@ public class TransactionController {
 
 	@Autowired
 	private TransactionService transactionService;
+
+	@GetMapping("/transactions")
+	public List<Transaction> findAll() {
+		return transactionService.findAll();
+	}
 }
--- bank/src/main/java/com/sandro/bank/bank/service/TransactionService.java
+++ bank/src/main/java/com/sandro/bank/bank/service/TransactionService.java
@@ -1,8 +1,11 @@
 package com.sandro.bank.bank.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sandro.bank.bank.entity.Transaction;
 import com.sandro.bank.bank.repository.TransactionRepository;
 
 @Service
@@ -10,5 +13,9 @@ public class TransactionService {
 
 	@Autowired
 	private TransactionRepository transactionRepository;
+
+	public List<Transaction> findAll() {
+		return transactionRepository.findAll();
+	}
 
 }
