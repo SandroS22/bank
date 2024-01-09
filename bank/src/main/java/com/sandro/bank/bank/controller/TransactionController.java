9e405001ff887ebeae02be64c0d62a3b717eb8d3
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-01-09 18:59:54 -0300

    feat: create mapping to get one transaction by id

--- bank/src/main/java/com/sandro/bank/bank/controller/TransactionController.java
+++ bank/src/main/java/com/sandro/bank/bank/controller/TransactionController.java
@@ -1,9 +1,12 @@
 package com.sandro.bank.bank.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +21,17 @@ public class TransactionController {
 	private TransactionService transactionService;
 
 	@GetMapping("/transactions")
-	public List<Transaction> findAll() {
+	public List<Transaction> findAllTransactions() {
 		return transactionService.findAll();
+	}
+
+	@GetMapping("/transactions/{id}")
+	public Object findTransactionById(@PathVariable Integer id) {
+		Optional<Transaction> transaction = transactionService.findById(id);
+		if (transaction.isEmpty()) {
+			return HttpStatus.BAD_REQUEST;
+		} else {
+			return transaction.get();
+		}
 	}
 }
