de8131d6664e9e9ffd9f1dd682cf2c30f69237c9
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-01-20 22:03:27 -0300

    feat: find by id

--- bank/src/main/java/com/sandro/bank/bank/controller/AccountController.java
+++ bank/src/main/java/com/sandro/bank/bank/controller/AccountController.java
@@ -1,8 +1,31 @@
 package com.sandro.bank.bank.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sandro.bank.bank.entity.Account;
+import com.sandro.bank.bank.service.AccountService;
+
 @RestController
+@RequestMapping("/api")
 public class AccountController {
+
+	@Autowired
+	private AccountService accountService;
+
+	@GetMapping("/account")
+	public Object findAccountById(Integer id) {
+		Optional<Account> account = accountService.findById(id);
+		if (account.isEmpty()) {
+			return HttpStatus.BAD_REQUEST;
+		} else {
+			return account.get();
+		}
+	}
 
 }
